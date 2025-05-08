0ecacc5bc4db62e1f4d95f4f235b02bbb908c4b5
cabrauck <carsten@bconsult.cc>
2025-05-08 15:19:29 +0000

    added sample navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<!-- src/routes/+layout.svelte -->
+<script>
+	export let data;
+</script>
+
+<nav class="bg-gray-800 text-white p-4">
+	<ul class="flex gap-4">
+		<li><a href="/" class="hover:underline">Home</a></li>
+		<li><a href="/about" class="hover:underline">Über mich</a></li>
+		<li><a href="/contact" class="hover:underline">Kontakt</a></li>
+		<li><a href="/impressum" class="hover:underline">Impressum</a></li>
+		<li><a href="/privacy" class="hover:underline">Datenschutz</a></li>
+	</ul>
+</nav>
+
+<main class="p-4">
+	<slot />
+</main>
